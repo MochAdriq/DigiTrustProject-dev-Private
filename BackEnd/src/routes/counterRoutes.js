2f3a958274323cd6ea01157682812abeb60d15b5
Moch Adriq <mochfadillah1208@gmail.com>
2025-10-27 19:05:38 +0700

    merubah nama file dan menghapus folder be

--- BackEnd/src/routes/counterRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/counterController");
-
-router.get("/:name", controller.getValue);
-router.post("/:name/increment", controller.increment);
-router.post("/:name/reset", controller.reset);
-
-module.exports = router;
